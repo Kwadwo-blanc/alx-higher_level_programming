ffe86d37e54eeb602d7ad9182df001d5e9c7f930
Kwadwo-blanc <ebenezerghart@gmail.com>
2023-12-07 19:11:11 -0800

    edit

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('a') - 1, -1):
-    print("{:c}".format(i), end="" if i % 2 == 0 else "")
+    print("{:c}".format(i),
+            if i % 2 == 0 else "{:c}".format(i).upper(), end="")
