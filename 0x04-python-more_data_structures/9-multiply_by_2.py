d708a81bf5da12e11071a6911549eac6871e09db
Kwadwo-blanc <ebenezerghart@gmail.com>
2023-12-08 17:21:39 -0800

    task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     res_dic = a_dictionary.copy()
-    list_keys = list(new_dir.keys())
+    list_keys = list(res_dic.keys())
 
     for a in list_keys:
-        new_dir[a] *= 2
+        res_dic[a] *= 2
 
     return res_dic
