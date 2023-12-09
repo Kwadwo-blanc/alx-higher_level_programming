4c735b86063c1414adf31eaf69a108bc10886278
Kwadwo-blanc <ebenezerghart@gmail.com>
2023-12-08 17:19:09 -0800

    task 9

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    res_dic = a_dictionary.copy()
+    list_keys = list(new_dir.keys())
+
+    for a in list_keys:
+        new_dir[a] *= 2
+
+    return res_dic
