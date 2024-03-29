4078714c3c33832ba1d0f63b99cd00fb92efe0ea
Kwadwo-blanc <ebenezerghart@gmail.com>
2024-03-29 15:47:18 -0700

    last

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Takes repository name and owner name as arguments, then
+lists 10 most recent commits of the repository by the user"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+
+    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        commits = response.json()[:10]  # Get 10 most recent commits
+        for commit in commits:
+            sha = commit['sha']
+            author_name = commit['commit']['author']['name']
+            print(f"{sha}: {author_name}")
+    else:
+        print("Error:", response.status_code)
