f9b9597dba50b5e005023a5ac17834531b90ebad
Kwadwo-blanc <ebenezerghart@gmail.com>
2024-03-29 17:30:58 -0700

    ninth

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script sends a request to a URL passed as an argument and displays only the status code of the response
-curl -s -o /tmp/response.txt -w "%{http_code}" "$1" && cat /tmp/response.txt
+# sends a request to a URL, and displays only the status code of the response.
+curl -s -o /dev/null -w "%{http_code}" "$1"
